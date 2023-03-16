8d7247bf3dbef6ffc70667e0e7d9d60d843ac041
Ben Cavins <ben.cavins@flatironschool.com>
2023-03-16 14:57:35 -0400

    do lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,41 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at).all()
+        messages_dict = [msg.to_dict() for msg in messages]
+        return make_response(jsonify(messages_dict), 200)
 
-@app.route('/messages/<int:id>')
+    elif request.method == 'POST':
+        new_msg_dict = request.get_json()
+        new_message = Message(
+            body=new_msg_dict['body'],
+            username=new_msg_dict['username']
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return make_response(jsonify(new_message.to_dict()), 201)
+
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    
+    if request.method == 'PATCH':
+        updates = request.get_json()
+        for field_name, value in updates.items():
+            setattr(message, field_name, value)
+        db.session.add(message)
+        db.session.commit()
+        return make_response(jsonify(message.to_dict()), 200)
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return make_response(jsonify({'status': 'delete success'}), 200)
+
 
 if __name__ == '__main__':
     app.run(port=5555)
